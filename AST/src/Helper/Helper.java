27aca4d4a4571710a64c1243c68150756d533cc6
zhanghuanzj <840796340@qq.com>
2015-10-04 19:59:18 +0800

    Begin to analyse communication depedence prefix and postfix changeable is finished

---
+++ AST/src/Helper/Helper.java
@@ -0,0 +1,12 @@
+package Helper;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.AnonymousClassDeclaration;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+public class Helper {
+
+
+}
